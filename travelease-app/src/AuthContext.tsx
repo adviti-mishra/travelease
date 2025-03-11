88add5768c510dacdabd231083a8d458aefab735
eshian <eshian@umich.edu>
2025-03-11 19:13:54 -0400

    added Google auth for history button

---
+++ travelease-app/src/AuthContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState } from "react";
+
+interface User {
+  name: string;
+  picture: string;
+  email: string;
+}
+
+interface AuthContextType {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
--- travelease-app/src/index.js
+++ travelease-app/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import MainApp from "./App";
+import { AuthProvider } from "./AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="1093233382771-dukv61smr5p0uq2b66shgq1l35633uqa.apps.googleusercontent.com">
-      <MainApp />
+        <AuthProvider>
+            <MainApp />
+        </AuthProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
