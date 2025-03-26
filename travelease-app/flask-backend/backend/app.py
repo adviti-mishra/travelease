ae8b1d3c4dfaf8858c726e27c1e00d44d46f49bd
Adviti Mishra <adviti@umich.edu>
2025-03-25 21:01:14 -0400

    Adde dpre-flight options for gnarly CORS stuff

--- travelease-app/flask-backend/backend/app.py
+++ travelease-app/flask-backend/backend/app.py
@@ -21,6 +21,17 @@ app = Flask(__name__)
 CORS(app, origins=["https://travelease-ow7tfdh80-adviti-mishras-projects.vercel.app"],
      allow_headers=["Content-Type", "Authorization"])
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
+@app.route('/api/process', methods=['OPTIONS'])
+def handle_options():
+    return '', 200
+
 # Serve React's index.html for the root
 @app.route("/")
 def serve_frontend():
